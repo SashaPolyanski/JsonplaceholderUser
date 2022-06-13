832b0ae495e872c20e5370efb943de0a520f52a9
SashaPolyanski <80836080+SashaPolyanski@users.noreply.github.com>
2022-06-13 16:58:31 +0300

    add library and data structure

---
+++ src/a1-main/b2-bll/store.ts
@@ -0,0 +1,23 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+
+const rootReducer = combineReducers({
+
+})
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+// все типы экшенов для App
+export type AppRootActionsType = any
+
+
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppRootActionsType>
+export const useAppDispatch = () => useDispatch<ThunkDispatch<AppRootStateType, unknown, AppRootActionsType>>()
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+
+
+// @ts-ignore
+window.store = store;
